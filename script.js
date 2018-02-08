1cf39b499169b4b9449e1cb17c25a7a0dfba0a59
Kostya Dombrovski <kostya.dombrovski@gmail.com>
2018-02-07 23:07:08 -0500

    Added basic login functionalities w/ admin account

--- script.js
+++ script.js
@@ -1,54 +1,35 @@
-var app = angular.module('ClouadBoard', []);
+var app = angular.module('CloudBoard', ['ngCookies']);
 
-app.service('loginService', function() {
+app.factory('loginService', ['$rootScope', '$cookies', '$cookieStore', function($rootScope, $cookies, $cookieStore) {
   var loggedIn = false;
   var user;
+  var logins = {'admin': 'admin'};
 
-  var logIn = function(userData) {
+  var logIn = function(username, password) {
+    if (!(logins[username] == password)) {
+      return false;
+    }
     loggedIn = true;
-    user = userData;
-    document.cookie = "loggedin=true;user=userData;";
+    user = username;
+    $cookieStore.put("loggedIn", "true");
+    $cookieStore.put("user", username);
+    $rootScope.$broadcast('loggingIn');
+    return true;
   };
 
   var logOut = function() {
     loggedIn = false;
     user = "";
-    document.cookie = "loggedin=false;";
+    $cookieStore.put("loggedIn", false);
   };
 
   var getLoginStatus = function() {
-    var name = "loggedin=";
-    var ca = document.cookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-      var c = ca[i];
-      while (c.charAt(0) == ' ') {
-          c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-          loggedInString = c.substring(name.length, c.length);
-          loggedIn = (loggedInString == "true") ? true : false;
-      }
-    }
-    return loggedIn;
+    return $cookieStore.get("loggedIn") || loggedIn;
   };
 
   var getUserName = function() {
-    if (!getLoginStatus()) {
-      return "";
-    }
-
-    var name = "user=";
-    var ca = document.cookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-      var c = ca[i];
-      while (c.charAt(0) == ' ') {
-          c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-          return c.substring(name.length, c.length);
-      }
-    }
-    return "";
+    //return $cookieStore.get("user");
+    return user;
   }
 
   return {
@@ -57,18 +38,32 @@ app.service('loginService', function() {
     getLoginStatus: getLoginStatus,
     getUserName: getUserName
   };
-});
-
-app.controller('boards', ['$scope', '$http', function($scope, $http, loginService) {
-  $scope.loggedIn = loginService.getLoginStatus();
-
 }]);
 
-app.controller('login', ['$scope', '$http', function($scope, $http, loginService) {
-  $scope.logIn = function(userData) {
-    loginService.logIn(userData);
+app.controller('boards', ['$scope', '$http', 'loginService', function($scope, $http, loginService) {
+  $scope.loggedIn = loginService.getLoginStatus();
+  $scope.name = loginService.getUserName();
+  $scope.boards = [];
+
+  $scope.$on('loggingIn', function() {
+    $scope.loggedIn = loginService.getLoginStatus();
+    $scope.name = loginService.getUserName();
+  })
+}]);
+
+app.controller('login', ['$scope', '$http', 'loginService', function($scope, $http, loginService) {
+  $scope.loggedIn = loginService.getLoginStatus();
+  $scope.username;
+  $scope.password;
+
+  $scope.logIn = function() {
+    var logInSuccessful = loginService.logIn($scope.username, $scope.password);
+    if (logInSuccessful != true) {
+      alert('Login failed');
+    }
+    $scope.loggedIn = loginService.getLoginStatus();
   };
-  $scope.logOut = function(userData) {
+  $scope.logOut = function() {
     loginService.logOut();
   }
 }]);
