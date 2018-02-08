3a7676755c9259bf63387f35f70e67c03fc4115d
Kostya Dombrovski <kostya.dombrovski@gmail.com>
2018-02-08 00:02:35 -0500

    Added basic board display

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
+function Board(id, hasContent, preview) {
+  this.id = id;
+  this.hasContent = hasContent;
+  this.preview = preview;
+}
+
 var app = angular.module('CloudBoard', ['ngCookies']);
 
 app.factory('loginService', ['$rootScope', '$cookies', '$cookieStore', function($rootScope, $cookies, $cookieStore) {
   var loggedIn = false;
-  var user;
+  var user = "admin";
   var logins = {'admin': 'admin'};
 
   var logIn = function(username, password) {
@@ -24,6 +30,7 @@ app.factory('loginService', ['$rootScope', '$cookies', '$cookieStore', function(
   };
 
   var getLoginStatus = function() {
+    //$rootScope.$broadcast('loggingIn');
     return $cookieStore.get("loggedIn") || loggedIn;
   };
 
@@ -48,7 +55,20 @@ app.controller('boards', ['$scope', '$http', 'loginService', function($scope, $h
   $scope.$on('loggingIn', function() {
     $scope.loggedIn = loginService.getLoginStatus();
     $scope.name = loginService.getUserName();
-  })
+    $scope.createBasicBoard();
+    $scope.createBlankBoard(2);
+    $scope.createBlankBoard(3);
+  });
+
+  $scope.createBasicBoard = function() {
+    var basicBoard = new Board(1, true, "some sample text");
+    $scope.boards.push(basicBoard);
+  };
+
+  $scope.createBlankBoard = function(id) {
+    var blankBoard = new Board(id, false, "");
+    $scope.boards.push(blankBoard);
+  };
 }]);
 
 app.controller('login', ['$scope', '$http', 'loginService', function($scope, $http, loginService) {
@@ -65,5 +85,5 @@ app.controller('login', ['$scope', '$http', 'loginService', function($scope, $ht
   };
   $scope.logOut = function() {
     loginService.logOut();
-  }
+  };
 }]);
