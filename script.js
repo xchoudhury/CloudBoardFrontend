c78c7a0cbaa7d6cd8d206bd7596e5a7b5e2734f3
Kostya Dombrovski <kostya.dombrovski@gmail.com>
2018-02-08 13:47:05 -0500

    Added basic copy and paste functionalities

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-function Board(id, hasContent, preview) {
+function Board(id, hasContent, preview, content) {
   this.id = id;
   this.hasContent = hasContent;
   this.preview = preview;
+  this.content = content;
+  this.pasting = false;
 }
 
 var app = angular.module('CloudBoard', ['ngCookies']);
@@ -47,7 +49,7 @@ app.factory('loginService', ['$rootScope', '$cookies', '$cookieStore', function(
   };
 }]);
 
-app.controller('boards', ['$scope', '$http', 'loginService', function($scope, $http, loginService) {
+app.controller('boards', ['$scope', '$http', '$window', 'loginService', function($scope, $http, $window, loginService) {
   $scope.loggedIn = loginService.getLoginStatus();
   $scope.name = loginService.getUserName();
   $scope.boards = [];
@@ -61,14 +63,55 @@ app.controller('boards', ['$scope', '$http', 'loginService', function($scope, $h
   });
 
   $scope.createBasicBoard = function() {
-    var basicBoard = new Board(1, true, "some sample text");
+    var basicBoard = new Board(1, true, "some sample text", "some sample text");
     $scope.boards.push(basicBoard);
   };
 
   $scope.createBlankBoard = function(id) {
-    var blankBoard = new Board(id, false, "");
+    var blankBoard = new Board(id, false, "", "");
     $scope.boards.push(blankBoard);
   };
+
+  $scope.copyFromBoard = function(board) {
+    if (!board.hasContent) {
+      return;
+    }
+    var textarea = document.createElement( "textarea" );
+    textarea.style.height = "0px";
+    textarea.style.left = "-100px";
+    textarea.style.opacity = "0";
+    textarea.style.position = "fixed";
+    textarea.style.top = "-100px";
+    textarea.style.width = "0px";
+    document.body.appendChild( textarea );
+    textarea.value = board.content;
+    textarea.select();
+    document.execCommand('copy');
+    textarea.parentNode.removeChild( textarea );
+    alert('Board ' + board.id + ' data copied! \nData: ' + board.content);
+  };
+
+  $scope.pasteToBoard = function(board) {
+    board.pasting = true;
+    board.hasContent = true;
+    setTimeout(function() {
+      document.getElementById(board.id + "pasting").focus();
+    }, 200);
+  };
+
+  $scope.keyCheck = function(e, board) {
+    if (e.keyCode == 13) {
+      $scope.savePaste(board);
+    }
+  };
+
+  $scope.savePaste = function(board) {
+    board.pasting = false;
+    board.hasContent = true;
+    board.preview = board.content;
+    // Send to server
+  };
+
 }]);
 
 app.controller('login', ['$scope', '$http', 'loginService', function($scope, $http, loginService) {
